fb0838af90f18800cda17ddea3e3211603fcdfd0
laura pedrosa <you@example.com>
2024-05-06 09:42:42 +0000

    Response

---
+++ vars/getResponse.groovy
@@ -0,0 +1,8 @@
+def call(String ip){
+       def get = new URL(${ip}).openConnection();
+            def getRC = get.getResponseCode();
+            println(getRC);
+            if(getRC.equals(200)){
+                println(get.getInputStream().getText());
+            } 
+}
