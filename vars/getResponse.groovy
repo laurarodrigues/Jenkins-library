28c1912a6a70eb08df2b7de6f8b451579c9a163a
laura pedrosa <you@example.com>
2024-05-06 09:56:06 +0000

    Response

--- vars/getResponse.groovy
+++ vars/getResponse.groovy
@@ -1,5 +1,7 @@
 def call(String ip){
-       def get = new URL(${ip}).openConnection();
+       //def get = new URL(${ip}).openConnection();
+        def get = new URL("http://10.105.136.231:80").openConnection();
+       
             def getRC = get.getResponseCode();
             println(getRC);
             if(getRC.equals(200)){
