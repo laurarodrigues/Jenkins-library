d0aae8168e7371708ee6031bb5d8d97f2fa8120f
laura pedrosa <you@example.com>
2024-05-07 09:54:10 +0000

    files

--- src/ApplicationFiles.groovy
+++ src/ApplicationFiles.groovy
@@ -16,12 +16,13 @@ public class ApplicationFiles{
 
         try{
             reader = new BufferedReader(new FileReader(propertiesFile));
+            System.out.println("Test");
             String line =reader.readLine();
 
             while(line != null){
                 System.out.println("Test" + line);
                 if(line.contains(property)){
-                    String[] result = line.split(".");
+                    String[] result = line.split("=");
                     deployFile = result[1];
                 }
                 line = reader.readLine();
