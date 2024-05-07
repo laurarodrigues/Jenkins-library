5aa605e9032df57cf1e879dacad559a7e8a23414
laura pedrosa <you@example.com>
2024-05-07 08:12:33 +0000

    AppFiles

---
+++ src/AppFiles.groovy
@@ -0,0 +1,27 @@
+def propertiesFile = libraryResource "application.properties"
+
+public class ApplicationFiles{
+
+
+    public ApplicationFiles(){
+    }
+
+    public String getProperty(String property){
+        String deployFile;
+        Scanner scanner = new Scanner(propertiesFile);
+        while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if(lines.contains(property)){
+                        String[] parts = lines.split("=");
+                        deployFile = parts[1];
+                }
+                // process the line
+        }
+        scanner.close();
+
+        return deployFile;
+    }
+
+    
+}
+
