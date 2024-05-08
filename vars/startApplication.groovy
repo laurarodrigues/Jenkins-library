58f2fc1ab6b257f723c2165508bbba529817b17f
laura pedrosa <you@example.com>
2024-05-08 07:29:24 +0000

    startApplication

--- vars/startApplication.groovy
+++ vars/startApplication.groovy
@@ -1,15 +1,9 @@
 def call(String databasePod){
     def propertiesFile = libraryResource "application_1.properties";
-    //sh "echo Datei mit Deployments"
-    sh "echo ${propertiesFile}"
-    sh "echo --------"
-    sh "echo Klasse ApplicationFile wird instanziert"
     def appFile = new ApplicationFiles(propertiesFile);
-    sh "echo Deployment-Datei für DB wird gesucht"
     String deployFileApp = appFile.property("database");
-    sh "echo gefunden ${deployFileApp}"
     applyDeployment(deployFileApp)
-    rolloutPod("postgres")
+    rolloutPod(databasePod)
     String deployFileDB = appFile.property("application");
     applyDeployment(deployFileDB)
 }
