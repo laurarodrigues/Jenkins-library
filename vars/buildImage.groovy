3cec02e8e55c9116a9c27ec0211839bd949add34
laura pedrosa <you@example.com>
2024-05-08 09:58:26 +0000

    buildImage

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,5 @@
 def call(){
-     def newImage = sh(script: 'docker manifest inspect lauraped/bigstart:latest')
+     def newImage = sh(script: 'docker manifest inspect lauraped/bigstart:latest', returnStdout: true)
     echo "${newImage}"
     if(newImage == null){
         sh 'docker build -t lauraped/bigstart:latest .'
